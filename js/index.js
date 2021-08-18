c02b8280d7646a247375ae3354e7715e7a8e99da
Rszooc <cursoszudemy@gmail.com>
2021-08-18 16:56:34 -0500

    Agregando la funcionalidad de sumar los checkbox

--- js/index.js
+++ js/index.js
@@ -1,3 +1,31 @@
 function testeoJava() {
     console.log('holi');
 }
+// Experimento para checkbox
+
+function sumaStreaming() {
+    let seleccionados = document.getElementById("miform").streaming;
+    let test = [];
+    console.log(seleccionados);
+    for (let i=0; i < seleccionados.length; i++) {
+        if (seleccionados[i].checked) {
+            test.push(Number(seleccionados[i].value));
+            // acumulador = acumulador + Number(checkboxes[x].value);
+            }
+        }
+    const acumular = (acumulador,numero) =>  acumulador+ numero;
+    const acumulado = test.reduce(acumular,0);
+    console.log(test);
+    console.log(acumulado);
+    return acumulado;
+}
+
+function peliculas (numMovies) {
+    return numMovies * 45;
+}
+
+function operacion1() {
+    const auxiliar = document.getElementById('pelicula');
+    const value = auxiliar.value;
+    console.log(`Valor: ${value}`);
+}
